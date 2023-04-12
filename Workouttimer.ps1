f7e90cb677310fd5a9eeb07a29a15a45989a58f9
Higgy345 <95872649+Higgy345@users.noreply.github.com>
2023-04-11 22:16:35 -0400

    A simple workout timer using TTS
    A simple workout timer using TTS for starting and stopping reps.

---
+++ Workouttimer.ps1
@@ -0,0 +1,20 @@
+﻿## Variables ##
+$endtime = (get-date).AddMinutes(10)
+$startexercise = "Start"
+$breakexercise = "Rest"
+$finished = "All Done"
+
+## Add Speech stuff ##
+Add-Type -AssemblyName System.Speech
+$Speech = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$Speech.SelectVoice("Microsoft Zira Desktop")
+
+## Meat and Potatoes ##
+While ($endtime -gt (get-date)){
+    $Speech.Speak($startexercise)
+    Sleep -Seconds 20
+    $Speech.Speak($breakexercise)
+    Sleep -Seconds 10
+    }
+$Speech.Speak($finished)
+
